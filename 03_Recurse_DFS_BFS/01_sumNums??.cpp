341927d9d942f691a9feba8b704383468ed73551
XueWang <13452916191@163.com>
2024-02-29 21:52:14 +0800

    03 Recursive, DFS and BFS

---
+++ 03_Recurse_DFS_BFS/01_sumNums??.cpp
@@ -0,0 +1,101 @@
+//
+//  01_sumNums.cpp
+//  AlgorithmProblem
+//
+//  Created by wangxue on 2020/9/6.
+//
+
+#include <iostream>
+using namespace std;
+//https://leetcode-cn.com/problems/qiu-12n-lcof/solution/qiu-12n-by-leetcode-solution/
+
+//求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句
+//通常实现递归的时候我们都会利用条件判断语句来决定递归的出口，但由于题目的限制我们不能使用条件判断语句，那么我们是否能使用别的办法来确定递归出口呢？答案就是逻辑运算符的短路性质。
+
+//以逻辑运算符 && 为例，对于 A && B 这个表达式，如果 A 表达式返回 False ，那么 A && B 已经确定为 False ，此时不会去执行表达式 B。同理，对于逻辑运算符 ||， 对于 A || B 这个表达式，如果 A 表达式返回 True ，那么 A || B 已经确定为 True ，此时不会去执行表达式 B
+
+//这个方法也被称作「俄罗斯农民乘法」，感兴趣的读者可以自行网上搜索相关资料。这个方法经常被用于两数相乘取模的场景
+
+int sumNums(int n){
+    n && (n += sumNums(n-1));
+    return n;
+}
+
+
+int quickMulti(int A, int B) {
+    int ans = 0;
+    for ( ; B; B >>= 1) {
+        if (B & 1) {
+            ans += A;
+        }
+        A <<= 1;
+    }
+    return ans;
+}
+
+
+class Solution {
+public:
+    int sumNums(int n) {
+        int ans = 0, A = n, B = n + 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        (B & 1) && (ans += A);
+        A <<= 1;
+        B >>= 1;
+
+        return ans >> 1;
+    }
+};
+
